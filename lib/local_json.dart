5bc7e53414bce27217f87d701a7a8efc2a15d1b1
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-03 16:11:49 +0300

    Update local_json.dart

--- lib/local_json.dart
+++ lib/local_json.dart
@@ -15,25 +15,60 @@ class _LocalJsonState extends State<LocalJson> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Local Jyson")),
-      body: Center(),
+      body: FutureBuilder<List<Country>>(
+        future: countryJysonRead(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            List<Country> countryList = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: countryList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(countryList[index].countryName),
+                  subtitle: Text(countryList[index].countryCapital),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Center();
+        }),
+      ),
     );
   }
 
-  countryJysonRead() async {
-    /*var */ String readString = await DefaultAssetBundle.of(context)
+  Future<List<Country>> countryJysonRead() async {
+    /*var */
+    String readString = await DefaultAssetBundle.of(context)
         .loadString("assets/daya/country.jyson");
     var jsonObject = jsonDecode(readString);
     /* debugPrint(readString);
+   
     // (jsonObject as List).map((e) => debugPrint(e.toString()));
+    
     //debugPrint(jsonObject.toString());
+    
     List countryList = jsonObject;
+   
     debugPrint(countryList[0]["President"][0].toString());*/
 
     List<Country> allcountry = (jsonObject as List)
         .map((countryMap) => Country.fromMap(countryMap))
         .toList();
+
     //debugPrint(allcountry[0].president);
 
     debugPrint(allcountry.length.toString());
+
+    return allcountry;
   }
 }
